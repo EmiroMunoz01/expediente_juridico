37dab43dbd81bffddbc9f2b74d4cd86d1eac2f88
EmiroMunoz01 <emiro_2001@icloud.com>
2025-03-09 18:04:12 -0500

    Se han implementado los cambios mas importantes a nivel de app, siendo un exito las peticiones mas importantes en la logica de la misma, de este modo se cierra este modulo de app.

--- frontend/juridico/src/app/asuntos-del-cliente-listado/asuntos-del-cliente-listado.component.ts
+++ frontend/juridico/src/app/asuntos-del-cliente-listado/asuntos-del-cliente-listado.component.ts
@@ -3,10 +3,11 @@ import { DatePipe, NgFor, NgIf } from '@angular/common';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { ClienteService } from '../servicios/cliente.service';
 import { Asunto } from '../modelo/asunto.interface';
+import { Cliente } from '../modelo/cliente.interface';
 
 @Component({
   selector: 'app-asuntos-del-cliente-listado',
-  imports: [RouterModule, NgFor, NgIf],
+  imports: [RouterModule, DatePipe],
   templateUrl: './asuntos-del-cliente-listado.component.html',
   styleUrl: './asuntos-del-cliente-listado.component.css',
 })
@@ -15,15 +16,31 @@ export default class AsuntosDelClienteListadoComponent implements OnInit {
 
   asuntos: Asunto[] = [];
 
+
+
   dniString = this.route.snapshot.paramMap.get('dni');
   dni = this.dniString ? Number(this.dniString) : null;
+
+  cliente: Cliente | undefined;
 
   private clientesService = inject(ClienteService);
 
   ngOnInit() {
     if (this.dni) {
       this.cargarAsuntos();
+      this.cargarCliente();
     }
+  }
+
+  cargarCliente() {
+    this.clientesService.obtenerClientePorDni(this.dni!).subscribe({
+      next: (data) => {
+        this.cliente = data; // Asegúrate de que 'asuntos' está definido en tu componente
+      },
+      error: (error) => {
+        console.error('Error al obtener los asuntos:', error);
+      },
+    });
   }
 
   cargarAsuntos() {
